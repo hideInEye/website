5737cab0ada402e6a6f149664bdace9b9d0633bf
juvenile <1050299953@qq.com>
2025-02-12 17:14:46 +0800

    文章删除添加

--- src/view/article/detail.vue
+++ src/view/article/detail.vue
@@ -1,21 +1,23 @@
 <template>
-  <div @click="changeArticle">
-    编辑文章
-  </div>
+  <div @click="changeArticle">编辑文章</div>
+  <div @click="deleteArticle">删除文章</div>
   <div>
-    {{ form.title || "暂无标题" }}
+    {{ form.title || '暂无标题' }}
   </div>
-  <div style="width:auto" v-html="form.content || '暂无内容'" class="content"></div>
+  <div
+    style="width: auto"
+    v-html="form.content || '暂无内容'"
+    class="content"
+  ></div>
 </template>
 
 <script lang="ts">
 import { defineComponent, onMounted, reactive } from 'vue';
-import { getDetail } from '@/axios/request';
+import { deletePosts, getDetail } from '@/axios/request';
 import { useRoute, useRouter } from 'vue-router';
 
 export default defineComponent({
   setup() {
-
     const route = useRoute();
     const router = useRouter();
 
@@ -47,9 +49,20 @@ export default defineComponent({
         },
       });
     };
+    const deleteArticle = () => {
+      console.log('删除文章');
+      if (!id) return;
+      deletePosts(id).then((res: any) => {
+        if (res.code > 300) {
+        } else {
+          router.replace('/');
+        }
+      });
+    };
     return {
       form,
-      changeArticle
+      changeArticle,
+      deleteArticle,
     };
   },
 });
